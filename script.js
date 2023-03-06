2944b6598af463d2b53386e646ab47e7941edd5f
shpilevoy777 <shpilevoy.ig@gmail.com>
2023-03-06 18:57:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// .........................task_#1.............................................
+// let a, b
+// a = +prompt ('insert a,', '1');
+// b = +prompt ('insert b', '1');
+// function maxNumber(a, b){
+//     if (a > b) {
+//        return (a);
+//     }
+//     else if (a === b){
+//         return ('a = b');
+//     }
+//       else {
+//         return (b);
+//      }
+// }
+// alert(maxNumber(a, b))
+
+// .........................task_#2.............................................
+// let number
+// number = +prompt ('Enter the number', '1')
+// function revers(number){
+//     number = number * -1
+//      return(number)
+// }
+// alert(revers(number))
+
+// .........................task_#3.............................................
+// let number, count
+// number = prompt ('Enter the number', '1')
+// count = prompt ('Enter the count', '1')
+// function three(number, count){
+//     number = number + 3 * count
+//     return (number)
+// }
+// alert(three(+number, +count));
+
+// .........................task_#4.............................................
+// let metric, km, m
+// km = +prompt ('enter distance in km', '1');
+// metric = prompt ('enter metric: "m" or "cm"', '1');
+// function getMetric(metric, km, m, cm){
+//     if (metric == "m"){
+//         return m(km)
+//     } else{
+//         return cm(km) 
+//     }
+// }
+
+// function KmToM(km){
+//     return(km * 1000);
+// }
+
+// function KmToCm(km){
+//     return(km * 100000);
+// }
+
+// alert(getMetric(metric, km, KmToM, KmToCm));
+
+
+
+
+// _________________________________________________________________________
+
+
+
+
+     
+
